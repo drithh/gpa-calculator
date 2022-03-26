2ba28c4b1032675c936e1c9002092bb485bf7ce0
Drithh <adrielalfeus@gmail.com>
2022-03-26 23:12:10 +0700

    add and remove row

--- script.js
+++ script.js
@@ -1,9 +1,98 @@
 let nilai = new Array();
 
+const toggleSelect = (selectMenu) => {
+  const button = selectMenu.querySelector('button');
+  const list = selectMenu.querySelector('ul');
+  if (button.classList.contains('show')) {
+    button.classList.remove('ring-blue-500');
+    button.classList.add('ring-secondary');
+  } else {
+    button.classList.remove('ring-secondary');
+    button.classList.add('ring-blue-500');
+  }
+  list.classList.toggle('hidden');
+  button.classList.toggle('show');
+};
+
+const addRow = () => {
+  const nama = document.querySelector('#mata-kuliah');
+  const sks = document.querySelector('#sks');
+  const nilai = document.querySelector('#nilai');
+
+  if (!nama.value) {
+    alert('Nama Mata Kuliah Tidak Boleh Kosong');
+  } else if (sks.innerHTML === 'Pilih SKS') {
+    alert('SKS Tidak Boleh Kosong');
+  } else if (nilai.innerHTML === 'Pilih Nilai') {
+    alert('Nilai Tidak Boleh Kosong');
+  } else {
+    const rowData = {
+      nama: document.querySelector('#mata-kuliah').value,
+      sks: sks.innerHTML.replace(/\s/g, ''),
+      nilai: nilai.innerHTML.replace(/\s/g, ''),
+    };
+
+    sks.innerHTML = 'Pilih SKS';
+    nilai.innerHTML = 'Pilih Nilai';
+
+    const row = setRow(rowData);
+    document.querySelector('.table-body').innerHTML += row;
+
+    console.log(rowData);
+  }
+};
+
+const deleteRow = (row) => {
+  row.parentNode.parentNode.remove();
+};
+
+const setRow = (rowData) => {
+  return `
+    <div
+      class="item-table flex h-20 px-4 place-items-center border-b-secondary border-b-[1px] border-opacity-50"
+    >
+      <div
+        class="font-Source text-base text-primary opacity-80 text-center w-[40%]"
+      >
+        ${rowData.nama}
+      </div>
+      <div
+        class="font-Source text-base text-primary opacity-80 text-center w-[25%]"
+      >
+        ${rowData.sks}
+      </div>
+      <div
+        class="font-Source text-base text-primary opacity-80 text-center w-[25%]"
+      >
+        ${rowData.nilai}
+      </div>
+      <div
+        class="font-Source text-base text-primary opacity-80 text-left w-[10%] flex place-content-center"
+      >
+        <button
+          class="hover:bg-gray-100 w-12 h-12 duration-500 rounded-full flex place-content-center place-items-center"
+          onclick="deleteRow(this);"
+        >
+          <svg fill="#424242" width="35" height="35" version="2.0">
+            <use href="#trash-bin" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  `;
+};
+
+const setSelectValue = (el) => {
+  const selectMenu = el.parentNode.parentNode;
+  selectMenu.querySelector('span').innerHTML = el.innerHTML;
+  toggleSelect(selectMenu);
+};
+
 const addNilai = () => {
   const wrapper = document.querySelector('.wrapper');
 
   const nama = wrapper.querySelector('#nama-mata-kuliah').value;
+
   const sks = wrapper.querySelector('#sks').value;
   const nilaiAngka = wrapper.querySelector('#nilai-angka').value;
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        PT: ['PT Sans', 'sans-serif'],
+        Josefin: ['Josefin Sans', 'sans-serif'],
+        Source: ['Source Sans Pro', 'sans-serif'],
+      },
+      colors: {
+        primary: '#424242',
+        secondary: '#aaaaaa',
+      },
+      letterSpacing: {
+        tightest: '-.075em',
+        tighter: '-.05em',
+        tight: '-.025em',
+        normal: '0',
+        wide: '.025em',
+        wider: '.05em',
+        widest: '.15em',
+      },
+    },
   },
   plugins: [],
 };
